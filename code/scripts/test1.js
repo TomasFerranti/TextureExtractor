ac8a808bb8ec7ef47d14f6a7917655d4559a3b5a
tomasferranti <tomas_ferranti_07@hotmail.com>
2020-12-09 00:48:44 -0300

    adding test image

--- code/scripts/test1.js
+++ code/scripts/test1.js
@@ -1,7 +1,20 @@
-//<![CDATA[
-var i = 10;
-if (i > 5) {
-  // some code
-  console.log(i);
-}
-//]]>
+var a = nj.array([2,3,4]);
+console.log(a.get(0))
+
+var imagem = document.getElementById("imagem_canvas");
+var ctxImagem = imagem.getContext("2d");
+
+var imgImagem = new Image();
+imgImagem.crossOrigin = '';
+imgImagem.src = "";
+
+imgImagem.onload = function(){
+	w=2*imgImagem.width;
+	h=2*imgImagem.height;
+	ctxImagem.drawImage(imgImagem, 0, 0,w,h); 
+};
+
+function updateImagem(){
+	x = prompt("Por favor insira a URL da imagem desejada.");
+	imgImagem.src = x;
+};
